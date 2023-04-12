d45962863333c12cb9766e024208054c80308cea
Vladimir Izmalkov <vladimir@vaticle.com>
2023-04-12 11:48:08 +0100

    Add comments

--- sample.js
+++ sample.js
@@ -7,19 +7,20 @@ async function main() {
     console.log("IAM Sample App");
 
     console.log("Connecting to the server");
-    const client = TypeDB.coreClient("0.0.0.0:1729");
+    const client = TypeDB.coreClient("0.0.0.0:1729"); // client is connected to the server
     console.log("Connecting to the `iam` database");
-    let session
+    let k; // define counter
+    let session // define session for later use
     try {
-        session = await client.session("iam", SessionType.DATA);
+        session = await client.session("iam", SessionType.DATA); // session is open
 
         console.log("");
         console.log("Request #1: User listing");
         let transaction;
         try {
-            transaction = await session.transaction(TransactionType.READ);
-            let match_query = "match $u isa user, has full-name $n, has email $e;";
-            let iterator = transaction.query.match(match_query);
+            transaction = await session.transaction(TransactionType.READ); // READ transaction is open
+            let match_query = "match $u isa user, has full-name $n, has email $e;"; // TypeQL query
+            let iterator = transaction.query.match(match_query); // Executing query
             let answers = await iterator.collect();
             let result = await Promise.all(
                 answers.map(answer =>
@@ -27,7 +28,7 @@ async function main() {
                      answer.map.get("e").value]
                 )
             );
-            k = 0;
+            k = 0; // reset the counter
             for(let i = 0; i < result.length; i++) {
                 k++;
                 console.log("User #" + k + ": " + result[i][0] + ", has E-mail: " + result[i][1]);
@@ -40,16 +41,16 @@ async function main() {
         console.log("");
         console.log("Request #2: Files that Kevin Morrison has access to");
         try {
-            transaction = await session.transaction(TransactionType.READ);
+            transaction = await session.transaction(TransactionType.READ); // READ transaction is open
             match_query = "match $u isa user, has full-name 'Kevin Morrison'; $p($u, $pa) isa permission; $o isa object, has path $fp; $pa($o, $va) isa access; get $fp;";
-            iterator = transaction.query.match(match_query);
+            iterator = transaction.query.match(match_query); // Executing query
             answers = await iterator.collect();
             result = await Promise.all(
                 answers.map(answer =>
                     [answer.map.get("fp").value]
                 )
             );
-            k = 0;
+            k = 0; // reset the counter
             for(let i = 0; i < result.length; i++) {
                 k++;
                 console.log("File #" + k + ": " + result[i]);
@@ -62,24 +63,24 @@ async function main() {
         console.log("");
         console.log("Request #3: Files that Kevin Morrison has view access to (with inference)");
         let options = TypeDBOptions.core();
-        options.infer = true;
+        options.infer = true; // set option to enable inference
         try {
-            transaction = await session.transaction(TransactionType.READ, options);
+            transaction = await session.transaction(TransactionType.READ, options); // READ transaction is open
             match_query = "match $u isa user, has full-name 'Kevin Morrison'; $p($u, $pa) isa permission; $o isa object, has path $fp; $pa($o, $va) isa access; $va isa action, has action-name 'view_file'; get $fp; sort $fp asc; offset 0; limit 5;"
-            iterator = transaction.query.match(match_query);
+            iterator = transaction.query.match(match_query); // Executing query
             answers = await iterator.collect();
             result = await Promise.all(
                 answers.map(answer =>
                     [answer.map.get("fp").value]
                 )
             );
-            k = 0;
+            k = 0; // reset the counter
             for(let i = 0; i < result.length; i++) {
                 k++;
                 console.log("File #" + k + ": " + result[i]);
             };
             match_query = "match $u isa user, has full-name 'Kevin Morrison'; $p($u, $pa) isa permission; $o isa object, has path $fp; $pa($o, $va) isa access; $va isa action, has action-name 'view_file'; get $fp; sort $fp asc; offset 5; limit 5;"
-            iterator = transaction.query.match(match_query);
+            iterator = transaction.query.match(match_query); // Executing query
             answers = await iterator.collect();
             result = await Promise.all(
                 answers.map(answer =>
@@ -99,21 +100,21 @@ async function main() {
         console.log("Request #4: Add a new file and a view access to it");
         const today = new Date(Date.now());
         try {
-            transaction = await session.transaction(TransactionType.WRITE);
+            transaction = await session.transaction(TransactionType.WRITE); // WRITE transaction is open
             let filepath = "logs/" + today.toISOString() + ".log";
             let insert_query = "insert $f isa file, has path '" + filepath + "';";
             console.log("Inserting file: " + filepath);
-            transaction.query.insert(insert_query);
+            transaction.query.insert(insert_query); // Executing query
             insert_query = "match $f isa file, has path '" + filepath + "'; $vav isa action, has action-name 'view_file'; insert ($vav, $f) isa access;";
             console.log("Adding view access to the file");
-            await transaction.query.insert(insert_query);
-            await transaction.commit();
+            await transaction.query.insert(insert_query); // Executing query
+            await transaction.commit(); // to persist changes, a 'write' transaction must be committed
         } finally {
         if (transaction.isOpen()) {await transaction.close()};
         };
     } finally {
-        await session?.close();
-        client.close();
+        await session?.close(); // close session
+        client.close(); // close server connection
     };
 };
 
