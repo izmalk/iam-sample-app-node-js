dde9167ee9bd562119f237bc9493f4595af4fdcc
Vladimir Izmalkov <vladimir@vaticle.com>
2023-12-01 10:50:04 +0000

    Update version and fix code

--- sample.js
+++ sample.js
@@ -1,18 +1,13 @@
-const { TypeDB } = require("typedb-client/TypeDB");
-const { SessionType } = require("typedb-client/api/connection/TypeDBSession");
-const { TransactionType } = require("typedb-client/api/connection/TypeDBTransaction");
-const { TypeDBOptions } = require("typedb-client/api/connection/TypeDBOptions");
-const { ThingType } = require("typedb-client/api/concept/type/ThingType");
-//const { Annotation } = ThingType.Annotation;
-//const { Annotation } = require("typedb-client/api/connection/TypeDBOptions");
-//import { Annotation } from 'typedb-client';
-const Annotation = ThingType.Annotation;
+const { TypeDB } = require("typedb-driver/TypeDB");
+const { SessionType } = require("typedb-driver/api/connection/TypeDBSession");
+const { TransactionType } = require("typedb-driver/api/connection/TypeDBTransaction");
+const { TypeDBOptions } = require("typedb-driver/api/connection/TypeDBOptions");
 
 async function main() {
     console.log("IAM Sample App");
 
     console.log("Connecting to the server");
-    const client = await TypeDB.coreClient("0.0.0.0:1729"); // client is connected to the server
+    const client = await TypeDB.coreDriver("0.0.0.0:1729"); // client is connected to the server
     console.log("Connecting to the `iam` database");
     let k; // define counter
     let session // define session for later use
@@ -24,13 +19,13 @@ async function main() {
         let transaction;
         try {
             transaction = await session.transaction(TransactionType.READ); // READ transaction is open
-            let match_query = "match $u isa user, has full-name $n, has email $e;"; // TypeQL query
-            let iterator = transaction.query.match(match_query); // Executing query
+            let match_query = "match $u isa user, has full-name $n, has email $e; get;"; // TypeQL query
+            let iterator = transaction.query.get(match_query); // Executing query
             let answers = await iterator.collect();
             let result = await Promise.all(
                 answers.map(answer =>
-                    [answer.map.get("n").value,
-                     answer.map.get("e").value]
+                    [answer.get("n").value,
+                     answer.get("e").value]
                 )
             );
             k = 0; // reset the counter
@@ -48,11 +43,11 @@ async function main() {
         try {
             transaction = await session.transaction(TransactionType.READ); // READ transaction is open
             match_query = "match $u isa user, has full-name 'Kevin Morrison'; $p($u, $pa) isa permission; $o isa object, has path $fp; $pa($o, $va) isa access; get $fp;";
-            iterator = transaction.query.match(match_query); // Executing query
+            iterator = transaction.query.get(match_query); // Executing query
             answers = await iterator.collect();
             result = await Promise.all(
                 answers.map(answer =>
-                    [answer.map.get("fp").value]
+                    [answer.get("fp").value]
                 )
             );
             k = 0; // reset the counter
@@ -67,16 +62,16 @@ async function main() {
 
         console.log("");
         console.log("Request #3: Files that Kevin Morrison has view access to (with inference)");
-        let options = TypeDBOptions.core();
+        let options = new TypeDBOptions();
         options.infer = true; // set option to enable inference
         try {
             transaction = await session.transaction(TransactionType.READ, options); // READ transaction is open
             match_query = "match $u isa user, has full-name 'Kevin Morrison'; $p($u, $pa) isa permission; $o isa object, has path $fp; $pa($o, $va) isa access; $va isa action, has name 'view_file'; get $fp; sort $fp asc; offset 0; limit 5;"
-            iterator = transaction.query.match(match_query); // Executing query
+            iterator = transaction.query.get(match_query); // Executing query
             answers = await iterator.collect();
             result = await Promise.all(
                 answers.map(answer =>
-                    [answer.map.get("fp").value]
+                    [answer.get("fp").value]
                 )
             );
             k = 0; // reset the counter
@@ -84,33 +79,14 @@ async function main() {
                 k++;
                 console.log("File #" + k + ": " + result[i]);
             };
-            //match_query = "match $u isa user, has full-name 'Kevin Morrison'; $p($u, $pa) isa permission; $o isa object, has path $fp; $pa($o, $va) isa access; $va isa action, has name 'view_file'; get $fp; sort $fp asc; offset 5; limit 5;"
-            match_query = "match $u isa user;"
-            iterator = transaction.query.match(match_query); // Executing query
+            match_query = "match $u isa user, has full-name 'Kevin Morrison'; $p($u, $pa) isa permission; $o isa object, has path $fp; $pa($o, $va) isa access; $va isa action, has name 'view_file'; get $fp; sort $fp asc; offset 5; limit 5;"
+            iterator = transaction.query.get(match_query); // Executing query
             answers = await iterator.collect();
-            //annotation_set = new Set();
-            //annotation_set.add(ThingType.Annotation.KEY)
-            //annotation_set.add(ThingType.Annotation.KEY)
-            results = await Promise.all(
+            result = await Promise.all(
                 answers.map(answer =>
-                    //[answer.map.get("fp").value]
-                    //answer.map.get("u").asThing().asRemote(transaction).getHas(annotations=annotation_set)
-                    //answer.map.get("u")
-                    answer.map.get("u").asThing().asRemote(transaction).getHas([Annotation.KEY])
+                    [answer.get("fp").value]
                 )
             );
-            /*attr = await Promise.all(
-                results(result =>
-                    result.asThing().asRemote(transaction).getHas()
-                )
-
-            );
-
-            for(let i = 0; i < attr.length; i++) {
-            console.log("-->" + attr[i])
-
-            }*/
-
             for(let i = 0; i < result.length; i++) {
                 k++;
                 console.log("File #" + k + ": " + result[i]);
